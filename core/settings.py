9c2b07fd038ced0534ad588dbdcdb2ed0c0d41f1
Daniel Rivera <danielriveravelasquez@gmail.com>
2024-09-05 23:20:30 -0500

    allowed hosts changes

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['localhost','danielriverav.up.railway.app','127.0.0.1', 'danielriverav.com']
+ALLOWED_HOSTS = ['localhost','danielriverav.up.railway.app','127.0.0.1', 'danielriverav.com', 'https://danielriverav.up.railway.app/']
 
 
 # Application definition
