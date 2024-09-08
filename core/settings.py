027bcb852f3605181711d0a20d9eb1830ccc2698
Daniel Rivera <danielriveravelasquez@gmail.com>
2024-09-08 14:29:22 -0500

    miss spelling in db code

--- core/settings.py
+++ core/settings.py
@@ -96,15 +96,15 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 if PRODUCTION:
     DATABASES = {
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-    }
+    }    
 
 
 
