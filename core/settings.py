075914b0f237bd6234ce557b52a1bfe993926729
Daniel Rivera <danielriveravelasquez@gmail.com>
2024-09-08 19:26:41 -0500

    normal working

--- core/settings.py
+++ core/settings.py
@@ -14,17 +14,9 @@ from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
 import os
-from django.contrib.auth.models import User
-
 
 load_dotenv()
 
-if not User.objects.filter(username=os.getenv('DJANGO_SUPERUSER_USERNAME')).exists():
-    User.objects.create_superuser(
-        os.getenv('DJANGO_SUPERUSER_USERNAME'),
-        os.getenv('DJANGO_SUPERUSER_EMAIL'),
-        os.getenv('DJANGO_SUPERUSER_PASSWORD')
-    )
 
 
 
