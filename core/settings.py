e74fd05efe7b03496b08fbf73e0cecb815f29152
Daniel Rivera <danielriveravelasquez@gmail.com>
2024-11-08 23:59:10 -0500

    some changes

--- core/settings.py
+++ core/settings.py
@@ -162,7 +162,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static')]
 # Otras configuraciones...
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'theme','media')
 
 
 
