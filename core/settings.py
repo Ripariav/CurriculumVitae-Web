021331517b14b54a603af83ed47329247c4be8cd
Daniel Rivera <danielriveravelasquez@gmail.com>
2024-11-09 22:42:56 -0500

    static and dynamic thigns

--- core/settings.py
+++ core/settings.py
@@ -162,7 +162,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static')]
 # Otras configuraciones...
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'theme','media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles','media')
 
 
 
