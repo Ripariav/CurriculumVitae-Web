55ae1a3f5eb9593f2df5894f11153d3059ba8c2c
Daniel Rivera <danielriveravelasquez@gmail.com>
2024-11-14 12:46:43 -0500

    media settings

--- core/settings.py
+++ core/settings.py
@@ -153,17 +153,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ 
-    BASE_DIR / "theme/static",  # Carpeta de archivos estáticos generada por Tailwind
+    BASE_DIR / "theme/static", 
+    BASE_DIR / "media/",  # Carpeta de archivos estáticos generada por Tailwind
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static')]
+STATICFILES_DIRS = [BASE_DIR / "theme/static"]
+
+#Media files
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+print(MEDIA_ROOT)
 
 # Otras configuraciones...
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
